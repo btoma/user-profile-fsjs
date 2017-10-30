6e64fe73fba7b68d1272ed7e0f36b69f077aff24
Biven <biven@lougeek.com>
2017-10-30 16:38:09 -0400

    modified the server file

--- serve/app.js
+++ serve/app.js
@@ -3,9 +3,8 @@
 var express = require('express');
 
 var app = express();
-
-app.use('/',express.static('src'));
 app.use('/node_modules',express.static('node_modules'));
+app.use('/',express.static('src'));
 
 app.listen(3000, function () {
   console.log('The server is running on 3000 port');
