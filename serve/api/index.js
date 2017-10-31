490bae491b7985556c1f3c97a98f35511982f48b
Biven Toma <biven@lougeek.com>
2017-10-31 19:58:49 -0400

    modified the serve and api file

--- serve/api/index.js
+++ serve/api/index.js
@@ -1,11 +1,13 @@
 'use strict';
+
 var express = require('express');
+var users = require('../../mock/users.json');
+
 var router = express.Router();
 
 router.get('/users', function (req,res) {
-  res.json({users:[]});
+  res.json({users: users});
 });
-
 
 //user: Add user to list.
 //user: edit user on list
@@ -13,3 +15,4 @@ router.get('/users', function (req,res) {
 //user: delete user from list
 
 
+module.exports = router;
--- serve/app.js
+++ serve/app.js
@@ -1,18 +1,11 @@
+
 'use strict';
 
 var express = require('express');
+var router = require('./api');
+
 var app = express();
-var router = express.Router();
-var users = require('/mock/users.json');
-router.get('/users', function (req,res) {
-  res.json({users: users});
-});
 
-
-//user: Add user to list.
-//user: edit user on list
-//user: update user on list
-//user: delete user from list
 app.use('/node_modules', express.static('node_modules'));
 app.use('/', express.static('src'));
 app.use('/api', router);
