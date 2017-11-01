2b1d0bc1309a9b236eb20e817d8f724b80429f67
Biven <biven@lougeek.com>
2017-11-01 09:31:12 -0400

    modified the database and added mongoos and schema for database

---
+++ serve/models/user.model.js
@@ -0,0 +1,31 @@
+// Load mongoose package
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+  full_name: String,
+  email: String,
+  address: String,
+  phone_number: String,
+  created_at: { type: Date, default: Date.now },
+});
+
+
+const User = mongoose.model('User', UserSchema);
+module.exports = User;
+
+
+User.count({}, function(err, count) {
+  if (err) {
+    throw err;
+  }
+
+  if (count > 0) return ;
+
+  const files = require('./file.seed.json');
+  User.create(files, function(err, newFiles) {
+    if (err) {
+      throw err;
+    }
+    console.log("DB seeded")
+  });
+});
